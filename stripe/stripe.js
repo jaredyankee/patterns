cfe499dab63f3ed7a179ef6925b330b4f17779d0
Jared Yankee <jayankee401@gmail.com>
2026-09-24 21:44:32 -0400

    add stripe webhook verificationm
    ## `verifyStripeHook`
    
    Verifies event emitted from stripe webhooks against signature
    
    ## `.env.example`
    -  added `stripe_webhook_secret`

--- stripe/stripe.js
+++ stripe/stripe.js
@@ -1,3 +1,20 @@
 // importing stripe
 import Stripe from 'Stripe';
 const stripe = new Stripe(process.env.stripe_api_key); // .env.example
+
+/**
+ *  Verifies you are the intended recipient. Show your credentials
+ * 
+ * @param {} event - emited from stripe webhook event
+ * @returns - validated request body
+ */
+async function verifyStripeHook (event) {
+    // test signature is header["stripe-signature"]
+    let signature = event.headers["stripe-signature"];
+
+    return await stripe.webhooks.constructEvent( 
+        event.body,
+        signature,
+        process.env.stripe_webhook_secret // .env.example 
+    )
+}
