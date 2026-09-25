5f11983becb898fdfd2dd0785e94e5336698ef33
Jared Yankee <jayankee401@gmail.com>
2026-09-25 08:33:40 -0400

    create `ns_auth`
    Performs OAath1 authorization for NetSuite requests using .env variables.

---
+++ netsuite/python/netsuite.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from requests_oauthlib import OAuth1
+from oauthlib.oauth1 import SIGNATURE_HMAC_SHA256
+
+# returns valid auth object for netsuite requests using .env creds
+# see .env.example
+def ns_auth ():
+    return OAuth1(
+        client_key=os.environ["ns_consumer_key"],
+        client_secret=os.environ["ns_consumer_secret"],
+        resource_owner_key=os.environ["ns_token_id"],
+        resource_owner_secret=os.environ["ns_token_secret"],
+        signature_method=SIGNATURE_HMAC_SHA256,
+        realm=os.environ["ns_realm"],
+    )
